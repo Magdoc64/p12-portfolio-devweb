196cc3725d20f7120e813493e58c562ca5e3c9b9
Magdoc64 <magcvdrby@gmail.com>
2025-05-18 15:48:26 +0200

    try 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({base: "/{p12-portfolio-devweb}",
   plugins: [react()],
 })
